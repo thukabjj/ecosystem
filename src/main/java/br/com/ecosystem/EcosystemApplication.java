44748c184027ef245ad20a818bb6d07b1fe4dd10
Arthur Costa <arthur.costa@trusthub.com.br>
2019-04-08 11:09:01 -0300

    Projeto de inicio spring

---
+++ src/main/java/br/com/ecosystem/EcosystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosystemApplication.class, args);
+	}
+
+}
